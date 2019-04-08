cb9738f49c0ce093223e8ffa37f055fb5469d278
Oleksandr Medvediev <oleksandrm@imagexmedia.com>
2019-04-08 18:39:41 +0300

    Medvediev-hw-7: update code.

--- hw_7/js/script.js
+++ hw_7/js/script.js
@@ -9,24 +9,35 @@ assignObjects({...}, {...}, false);
 
 function assignObjects(obj1, obj2, flag = false){
   newObj = {};
-  for (var i in obj1) {
-    if (obj1[i] !== undefined && obj2[i] !== undefined) {
-      newObj[i] = flag ? obj1[i] : obj2[i];
-    }
+  if (!flag) {
+    newObj = fillNewObject(obj1, newObj);
+    newObj = fillNewObject(obj2, newObj);
+  }
+  else {
+    newObj = fillNewObject(obj2, newObj);
+    newObj = fillNewObject(obj1, newObj);
+  }
+
+  return newObj;
+}
+
+function fillNewObject(obj, newObj = {}) {
+  for (var i in obj) {
+    newObj[i] = obj[i];
   }
   return newObj;
 }
 
-res1 = assignObjects({ x: 10, y: 20 }, { z: 30 }, false);
+res0 = assignObjects({ x: 10, y: 20 }, { z: 30 }, false);
+res1 = assignObjects({ x: 10, y: 20 }, { z: 30 }, true);
 res2 = assignObjects({ x: 10 }, { x: 20, y: 30 }, true);
 res3 = assignObjects({ x: 10 }, { x: 20, y: 30 }, false);
+console.log(res0);
 console.log(res1);
 console.log(res2);
 console.log(res3);
 
-/*
-Адаптировать задачу #1 из домашней работы под неопределенное количество аргументов. Последний аргумент true || false
- */
+/*Адаптировать задачу #1 из домашней работы под неопределенное количество аргументов. Последний аргумент true || false*/
 
 function assignObjectsLong() {
   var args = Array.prototype.slice.call(arguments);
@@ -37,10 +48,13 @@ function assignObjectsLong() {
   for (let index = 0; index < objects.length - 1; index++) {
     let obj1 = objects[index];
     let obj2 = objects[index + 1];
-    for (var i in obj1) {
-      if (obj1[i] !== undefined && obj2[i] !== undefined) {
-        newObj[i] = flag ? obj1[i] : obj2[i];
-      }
+    if (!flag) {
+      newObj = fillNewObject(obj1, newObj);
+      newObj = fillNewObject(obj2, newObj);
+    }
+    else {
+      newObj = fillNewObject(obj2, newObj);
+      newObj = fillNewObject(obj1, newObj);
     }
   }
 
